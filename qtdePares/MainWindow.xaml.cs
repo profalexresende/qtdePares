36bd6cbd5a508bf7eb5b6b8a6a7630f394980df1
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-10-04 11:57:37 -0300

    Projeto Qtde de números pares em um intervalo

---
+++ qtdePares/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace qtdePares
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFatorial_Click(object sender, RoutedEventArgs e)
+        {
+            double num1, num2,cont;
+            int qtdePares=0;
+
+            num1 = Convert.ToDouble(txtNumero1.Text);
+            num2 = Convert.ToDouble(txtNumero2.Text);
+            if (num1 < num2)
+            {
+                cont = num1;
+
+                do
+                {
+                    if (cont % 2 == 0)
+                    {
+                        qtdePares = qtdePares + 1;
+                    }
+                    cont++;
+                } while (cont <= num2);
+                lblResultado.Content = qtdePares.ToString();
+            }
+        }
+    }
+}
